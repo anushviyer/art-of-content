0729dc8c35023964d550990c8ec088a77b974051
anushviyer <anushviyer@gmail.com>
2026-03-26 17:50:22 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Portfolio from './components/Portfolio';
+import WhyChooseUs from './components/WhyChooseUs';
+import Process from './components/Process';
+import CTA from './components/CTA';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="scroll-smooth">
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Portfolio />
+      <WhyChooseUs />
+      <Process />
+      <CTA />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
